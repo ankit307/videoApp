ce62a3c33d2281feff224b59d5746930e4859c5e
Ankit <ankitgarg307@gmail.com>
2019-07-07 04:59:14 -0400

    Google analytics

--- src/App.js
+++ src/App.js
@@ -3,8 +3,14 @@ import {SearchBar} from './components/searchBar';
 import VideoList from './components/videoList';
 import VideoDetail from './components/videoDetail';
 import YouTube from './api/youtube';
+import uuidv1 from 'uuid/v1';
+import ReactGA from 'react-ga';
 const API_KEY="AIzaSyC_mSAS-Py2Lc1edNp-SdiiBzzZf48Y0OM";
-
+const trackingId = "UA-64038998-1"; // Replace with your Google Analytics tracking ID
+ReactGA.initialize(trackingId);
+ReactGA.set({
+  userId: uuidv1(),
+ })
  class App extends React.Component{
   state={
     videoList:[],
@@ -14,14 +20,20 @@ const API_KEY="AIzaSyC_mSAS-Py2Lc1edNp-SdiiBzzZf48Y0OM";
   loadYouTubeVideos=async (term)=>{
       let data=await YouTube.get('/search',{ params: { q: term ,part:'snippet', maxResults: 5,  key: API_KEY
       }})
-      console.log(data);  
       this.setState({
         videoList:data.data.items,
         selectedVideo:data.data.items[0]
       })
+    ReactGA.event({
+       category: "Searched Video",
+       action: "User searched for video: "+term,
+    });
     }
     onVideoSelect=(video)=>{
-      console.log(video)
+    ReactGA.event({
+      category: "Selected Video",
+      action: "User selected video: "+video.snippet.title,
+        });
       this.setState({
         selectedVideo:video
       })
